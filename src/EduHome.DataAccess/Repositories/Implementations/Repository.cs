41b8599a4cd23aa4e80b8833cbe0dac28dd4284c
LaleSmdva <6vrseyy@code.edu.az>
2023-01-09 23:53:11 +0400

    rep

---
+++ src/EduHome.DataAccess/Repositories/Implementations/Repository.cs
@@ -0,0 +1,32 @@
+﻿using EduHome.Core.Entities;
+using EduHome.Core.Interfaces;
+using EduHome.DataAccess.Contexts;
+using EduHome.DataAccess.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduHome.DataAccess.Repositories.Implementations
+{
+	public class Repository<T> : IRepository<T> where T : class, IEntity, new()
+	{
+		private readonly AppDbContext _context;
+		//private readonly DbSet<T> _table;
+
+		public Repository(AppDbContext context/*, DbSet<T> table*/)
+		{
+			_context = context;
+			//_table = table;
+		}
+
+		public DbSet<T> Table { get; set; }
+
+		public Task<IEnumerable<T>> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/EduHome.DataAccess/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using EduHome.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduHome.DataAccess.Repositories.Interfaces;
+
+public interface IRepository<T> where T:class,IEntity,new()
+{
+	 Task<IEnumerable<T>> GetAll();
+     DbSet<T> Table { get; set; } 
+}
+
